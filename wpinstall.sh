748cf49b7ede5871961ab6e6353f04057fa6634d
ptrjs <peterjose64@gmail.com>
2022-10-26 16:27:39 +0700

    script db

--- wpinstall.sh
+++ wpinstall.sh
@@ -5,13 +5,16 @@ sudo apt install nginx mariadb-server mariadb-client php-fpm php-mysql
 
 sudo mysql_secure_installation
 
-#pengisian database masih menunggu
-#
-#
-#
-#
-#
-#
+#pengisian database 
+
+sudo mysql -uroot -proot -e "CREATE DATABASE tes;"
+
+sudo mysql -uroot -proot -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';"
+
+sudo mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON tes.* to user@'localhost';"
+
+sudo mysql -uroot -proot -e "FLUSH PRIVILEGES;"
+
 
 #bikin file di directory /etc/nginx/sites-available/wordpress untuk mengisi config
 #file config dibuat terpisah diluar script bash
