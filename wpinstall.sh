6cde7bec3d2a1f2759e4b7b462e665c99b6d72fc
ptrjs <peterjose64@gmail.com>
2022-10-26 16:33:48 +0700

    script penghapusan dan pemindahan isi config

--- wpinstall.sh
+++ wpinstall.sh
@@ -6,7 +6,6 @@ sudo apt install nginx mariadb-server mariadb-client php-fpm php-mysql
 sudo mysql_secure_installation
 
 #pengisian database 
-
 sudo mysql -uroot -proot -e "CREATE DATABASE tes;"
 
 sudo mysql -uroot -proot -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';"
@@ -18,7 +17,12 @@ sudo mysql -uroot -proot -e "FLUSH PRIVILEGES;"
 
 #bikin file di directory /etc/nginx/sites-available/wordpress untuk mengisi config
 #file config dibuat terpisah diluar script bash
-
 sudo cp /home/peter/config /etc/nginx/sites-available/wordpress
+
+
+
+#penghapusan config default dan pemindahan config yang sudah dimodif 
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/wordpress
 
 
