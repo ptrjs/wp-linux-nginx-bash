9392e2a0e632642a9f971c6890b588ba60f51049
ptrjs <peterjose64@gmail.com>
2022-10-26 14:58:09 +0700

    script untuk mengisi isi config ke dalam wordpress

--- wpinstall.sh
+++ wpinstall.sh
@@ -2,3 +2,21 @@
 
 sudo apt update
 sudo apt install nginx mariadb-server mariadb-client php-fpm php-mysql
+
+sudo mysql_secure_installation
+
+#pengisian database masih menunggu
+#
+#
+#
+#
+#
+#
+
+#bikin file di directory /etc/nginx/sites-available/wordpress untuk mengisi config
+#file config dibuat terpisah diluar script bash
+
+sudo touch /etc/nginx/sites-available/wordpress
+sudo cp /home/peter/config /etc/nginx/sites-available/wordpress
+
+
